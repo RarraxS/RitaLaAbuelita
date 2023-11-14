71ff8eec4a5ec6a2684ad581b28dfdef76f7ca32
RarraxS <itisrarra@gmail.com>
2023-11-14 21:51:46 +0100

    V 0.1.1.2
    Ya no entra en el bucle infinito

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject canvasDebug, canvasInicio, canvasControles;
+
     public string controles;
     public bool permitirAbrirMenuControles = true;
 
@@ -19,8 +21,74 @@ public class GameManager : MonoBehaviour
         if (instance == null)
             instance = this;
         else
-            Destroy(this);
+            Destroy(gameObject);
 
         DontDestroyOnLoad(this);
+
+        canvasDebug.SetActive(false);
+        canvasInicio.SetActive(true);
+        canvasControles.SetActive(false);
+    }
+
+    void Update()
+    {
+        MenuDebug();
+    }
+
+    //Debug
+    void MenuDebug()
+    {
+        if ((Input.GetKeyDown(KeyCode.Escape)) && !canvasDebug.activeSelf)
+        {
+            canvasDebug.SetActive(true);
+        }
+
+        else if ((Input.GetKeyDown(KeyCode.Escape)) && canvasDebug.activeSelf)
+        {
+            canvasDebug.SetActive(false);
+        }
+    }
+
+    public void TituloDebug()
+    {
+        canvasDebug.SetActive(false);
+        SceneManager.LoadScene("Titulo");
+    }
+
+    public void MinijuegoPulsarIngredientesDebug()
+    {
+        canvasDebug.SetActive(false);
+        SceneManager.LoadScene("PulsarIngredientes");
+    }
+
+    //Fin Debug
+
+    public void Jugar()
+    {
+        SceneManager.LoadScene("Pueblo");
+    }
+
+    public void AbrirMenuControles()
+    {
+        canvasInicio.SetActive(false);
+        canvasControles.SetActive(true);
+    }
+
+    void CerrarMenuControles()
+    {
+        canvasControles.SetActive(false);
+        canvasInicio.SetActive(true);
+    }
+
+    public void botonDiestro()
+    {
+        GameManager.Instance.controles = "diestro";
+        CerrarMenuControles();
+    }
+
+    public void botonZurdo()
+    {
+        GameManager.Instance.controles = "zurdo";
+        CerrarMenuControles();
     }
 }
--- Assets/Scripts/Pueblo/PuebloManager.cs
+++ Assets/Scripts/Pueblo/PuebloManager.cs
@@ -5,63 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class PuebloManager : MonoBehaviour
 {
-    [SerializeField] public GameObject canvasDebug, canvasControles;
-
-    void Awake()
-    {
-        canvasDebug.SetActive(false);
-    }
-
-    void Start()
-    {
-        if (GameManager.Instance.permitirAbrirMenuControles == true)
-        {
-            canvasControles.SetActive(true);
-            GameManager.Instance.permitirAbrirMenuControles = false;
-        }
-    }
 
     void Update()
     {
-        MenuDebug();
-
         CambioEscenaDemo();
-    }
-
-    public void MinijuegoPulsarIngredientes()
-    {
-        canvasDebug.SetActive(false);
-        SceneManager.LoadScene("PulsarIngredientes");
-    }
-
-    void CerrarMenuControles()
-    {
-        canvasControles.SetActive(false);
-    }
-
-    public void botonDiestro()
-    {
-        GameManager.Instance.controles = "diestro";
-        CerrarMenuControles();
-    }
-
-    public void botonZurdo()
-    {
-        GameManager.Instance.controles = "zurdo";
-        CerrarMenuControles();
-    }
-
-    void MenuDebug()
-    {
-        if ((Input.GetKeyDown(KeyCode.Escape)) && !canvasDebug.activeSelf)
-        {
-            canvasDebug.SetActive(true);
-        }
-
-        else if ((Input.GetKeyDown(KeyCode.Escape)) && canvasDebug.activeSelf)
-        {
-            canvasDebug.SetActive(false);
-        }
     }
 
     void CambioEscenaDemo()
