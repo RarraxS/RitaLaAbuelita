17268f6ead8de96f13ec11bc44eb738bf4306df5
informaticaesne <informatica@esne.es>
2023-11-20 13:45:35 +0100

    Beta 2 en proceso

--- Assets/Scripts/Pueblo/DialogoNpc.cs
+++ Assets/Scripts/Pueblo/DialogoNpc.cs
@@ -2,17 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DialogoNpc : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+[CreateAssetMenu(menuName = "Diálogos/Diálogos con NPC")]
+public class DialogoNpc : ScriptableObject
+{
+    public GameObject npc;
+    public string dialogo;
+    public GameObject canvasDialogo;
 }
--- Assets/Scripts/Pueblo/Rita.cs
+++ Assets/Scripts/Pueblo/Rita.cs
@@ -18,30 +18,60 @@ public class Rita : MonoBehaviour
     {
         Movimiento();
     }
+
     void Movimiento()
     {
+        Vector2 direccion = Vector2.zero;
+
         if ((Input.GetKey(KeyCode.I) && GameManager.Instance.controles == "zurdo") ||
             (Input.GetKey(KeyCode.W) && GameManager.Instance.controles == "diestro"))
         {
-            rb.position += (Vector2)(Time.deltaTime * velocidad * transform.up);//Alante
+            direccion += new Vector2(transform.up.x, transform.up.y); // Alante
         }
 
         if ((Input.GetKey(KeyCode.K) && GameManager.Instance.controles == "zurdo") ||
             (Input.GetKey(KeyCode.S) && GameManager.Instance.controles == "diestro"))
         {
-            rb.position += (Vector2)(-transform.up * velocidad * Time.deltaTime);//Atrás
+            direccion += new Vector2(-transform.up.x, -transform.up.y); // Atrás
         }
 
         if ((Input.GetKey(KeyCode.J) && GameManager.Instance.controles == "zurdo") ||
             (Input.GetKey(KeyCode.A) && GameManager.Instance.controles == "diestro"))
         {
-            rb.position += (Vector2)(-transform.right * velocidad * Time.deltaTime);//Izquierda
+            direccion += new Vector2(-transform.right.x, -transform.right.y); // Izquierda
         }
 
         if ((Input.GetKey(KeyCode.L) && GameManager.Instance.controles == "zurdo") ||
             (Input.GetKey(KeyCode.D) && GameManager.Instance.controles == "diestro"))
         {
-            rb.position += (Vector2)(transform.right * velocidad * Time.deltaTime);//Derecha
+            direccion += new Vector2(transform.right.x, transform.right.y); // Derecha
         }
+
+        // Normalizar el vector de dirección si es diferente de cero
+        if (direccion != Vector2.zero)
+        {
+            direccion.Normalize();
+        }
+
+        // Convertir la posición actual a Vector2 y aplicar la velocidad
+        Vector2 nuevaPosicion = rb.position;
+        nuevaPosicion += direccion * velocidad * Time.deltaTime;
+
+        // Asignar la nueva posición al Rigidbody
+        rb.position = nuevaPosicion;
+    }
+
+    void ApuntarConRaycast()
+    {
+        // Lanzar un rayo en la dirección del movimiento
+        RaycastHit2D hit = Physics2D.Raycast(rb.position, rb.velocity.normalized);
+
+        // Puedes realizar acciones basadas en la información del rayo, por ejemplo, imprimir el objeto golpeado
+        if (hit.collider != null)
+        {
+            Debug.Log("Objeto golpeado: " + hit.collider.name);
+        }
+
+        // Aquí puedes hacer más cosas con la información del rayo si es necesario
     }
 }
