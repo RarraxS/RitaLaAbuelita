5b6f2cfa270d1e31d6af336703e9dbbf4b67b5d5
RarraxS <itisrarra@gmail.com>
2023-12-30 03:17:38 +0100

    Botones de vuelta al pueblo

---
+++ Assets/Scripts/BotonAjustes.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotonAjustes : MonoBehaviour
+{
+    public void MenuControles()
+    {
+        GameManager.Instance.AbrirMenuControles();
+    }
+}
