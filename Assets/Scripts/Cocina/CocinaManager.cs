810b862eadb450483989e48ecb1ce7b16f892894
Beacevi <beacevi@gmail.com>
2024-01-03 14:36:22 +0100

    Casi terminado la cocina

--- Assets/Scripts/Cocina/CocinaManager.cs
+++ Assets/Scripts/Cocina/CocinaManager.cs
@@ -5,14 +5,11 @@ using UnityEngine.Video;
 
 public class CocinaManager : MonoBehaviour
 {
-    //Objeto principal
-    [SerializeField] GameObject Cuchillo;
-
-    //Objetos condicionales
-    [SerializeField] GameObject Correcto1;
-    [SerializeField] GameObject Correcto2;
-    [SerializeField] GameObject Correcto3;
-    [SerializeField] GameObject Correcto4;
+ 
+    //[SerializeField] GameObject Correcto1;
+    //[SerializeField] GameObject Correcto2;
+    //[SerializeField] GameObject Correcto3;
+    //[SerializeField] GameObject Correcto4;
 
     //Atributos
     public int vidas = 3;
@@ -22,7 +19,6 @@ public class CocinaManager : MonoBehaviour
     [SerializeField] GameObject canvasWinGame;
     [SerializeField] GameObject Boton;
 
-    // Start is called before the first frame update
     void Start()
     {
         canvasGameOver.SetActive(false);
@@ -36,17 +32,97 @@ public class CocinaManager : MonoBehaviour
         
     }
 
-    public void Cortar(Collider2D collision)
+    //public void Cortar(Collider2D collision)
+    //{
+    //    if (collision.tag == "Correcto") //Que sucede con Correcto
+    //    {
+    //        //Animacion de cortar
+    //        Debug.Log("Acertaste");
+    //    }
+
+    //    if (collision.tag == "Incorrecto") //Que sucede con Correcto
+    //    {
+    //        vidas--;
+    //        Debug.Log("Fallaste");
+    //    }
+    //}
+
+    public void OnTriggerStay2D(Collider2D collision)
     {
-        if(collision)
+        if (collision.gameObject.CompareTag("Correcto1")) //Que sucede con Correcto
         {
             //Animacion de cortar
-            Debug.Log("Acertaste ");
+            Destroy(collision.gameObject);
+            Debug.Log("Acertaste");
         }
-        else 
+        if (collision.gameObject.CompareTag("Correcto2")) //Que sucede con Correcto
+        {
+            //Animacion de cortar
+            Destroy(collision.gameObject);
+            Debug.Log("Acertaste");
+        }
+        if (collision.gameObject.CompareTag("Correcto3")) //Que sucede con Correcto
+        {
+            //Animacion de cortar
+            Destroy(collision.gameObject);
+            Debug.Log("Acertaste");
+        }
+        if (collision.gameObject.CompareTag("Correcto4")) //Que sucede con Correcto
+        {
+            //Animacion de cortar
+            Destroy(collision.gameObject);
+            Debug.Log("Acertaste");
+        }
+
+        if (collision.gameObject.CompareTag("Incorrecto1")) //Que sucede con Incorrecto
         {
             vidas--;
-            Debug.Log("Acertaste ");
+            Debug.Log("Fallaste");
+
+            if (vidas <= 0)
+            {
+                Morir();
+            }
+        }
+        if (collision.gameObject.CompareTag("Incorrecto2")) //Que sucede con Incorrecto
+        {
+            vidas--;
+            Debug.Log("Fallaste");
+
+            if (vidas <= 0)
+            {
+                Morir();
+            }
+        }
+        if (collision.gameObject.CompareTag("Incorrecto3")) //Que sucede con Incorrecto
+        {
+            vidas--;
+            Debug.Log("Fallaste");
+
+            if (vidas <= 0)
+            {
+                Morir();
+            }
+        }
+        if (collision.gameObject.CompareTag("Incorrecto4")) //Que sucede con Incorrecto
+        {
+            vidas--;
+            Debug.Log("Fallaste");
+
+            if (vidas <= 0)
+            {
+                Morir();
+            }
+        }
+        if (collision.gameObject.CompareTag("Incorrecto5")) //Que sucede con Incorrecto
+        {
+            vidas--;
+            Debug.Log("Fallaste");
+
+            if (vidas <= 0)
+            {
+                Morir();
+            }
         }
     }
     public void Morir()
@@ -57,4 +133,5 @@ public class CocinaManager : MonoBehaviour
             Boton.gameObject.SetActive(false);
         }
     }
+
 }
