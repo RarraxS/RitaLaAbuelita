35dc420b4d395c09a1c64cddaa913f0303082b4e
informaticaesne <informatica@esne.es>
2024-01-16 15:51:24 +0100

    Cocina terminada

--- Assets/Scripts/Cocina/CocinaManager.cs
+++ Assets/Scripts/Cocina/CocinaManager.cs
@@ -15,123 +15,123 @@ public class CocinaManager : MonoBehaviour
     public int vidas = 3;
 
     //Canvas
-    [SerializeField] GameObject canvasGameOver;
-    [SerializeField] GameObject canvasWinGame;
-    [SerializeField] GameObject Boton;
+    //[SerializeField] GameObject canvasGameOver;
+    //[SerializeField] GameObject canvasWinGame;
+    //[SerializeField] GameObject Boton;
 
-    void Start()
-    {
-        canvasGameOver.SetActive(false);
-        canvasWinGame.SetActive(false);
-        Boton.gameObject.SetActive(true);
-    }
+    //void Start()
+    //{
+    //    canvasGameOver.SetActive(false);
+    //    canvasWinGame.SetActive(false);
+    //    Boton.gameObject.SetActive(true);
+    //}
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Morir();
     }
 
-    //public void Cortar(Collider2D collision)
+    ////public void Cortar(Collider2D collision)
+    ////{
+    ////    if (collision.tag == "Correcto") //Que sucede con Correcto
+    ////    {
+    ////        //Animacion de cortar
+    ////        Debug.Log("Acertaste");
+    ////    }
+
+    ////    if (collision.tag == "Incorrecto") //Que sucede con Correcto
+    ////    {
+    ////        vidas--;
+    ////        Debug.Log("Fallaste");
+    ////    }
+    ////}
+
+    //public void OnTriggerStay2D(Collider2D collision)
     //{
-    //    if (collision.tag == "Correcto") //Que sucede con Correcto
+    //    if (collision.gameObject.CompareTag("Correcto1")) //Que sucede con Correcto
     //    {
     //        //Animacion de cortar
+    //        Destroy(collision.gameObject);
+    //        Debug.Log("Acertaste");
+    //    }
+    //    if (collision.gameObject.CompareTag("Correcto2")) //Que sucede con Correcto
+    //    {
+    //        //Animacion de cortar
+    //        Destroy(collision.gameObject);
+    //        Debug.Log("Acertaste");
+    //    }
+    //    if (collision.gameObject.CompareTag("Correcto3")) //Que sucede con Correcto
+    //    {
+    //        //Animacion de cortar
+    //        Destroy(collision.gameObject);
+    //        Debug.Log("Acertaste");
+    //    }
+    //    if (collision.gameObject.CompareTag("Correcto4")) //Que sucede con Correcto
+    //    {
+    //        //Animacion de cortar
+    //        Destroy(collision.gameObject);
     //        Debug.Log("Acertaste");
     //    }
 
-    //    if (collision.tag == "Incorrecto") //Que sucede con Correcto
+    //    if (collision.gameObject.CompareTag("Incorrecto1")) //Que sucede con Incorrecto
     //    {
     //        vidas--;
     //        Debug.Log("Fallaste");
+
+    //        if (vidas <= 0)
+    //        {
+    //            Morir();
+    //        }
+    //    }
+    //    if (collision.gameObject.CompareTag("Incorrecto2")) //Que sucede con Incorrecto
+    //    {
+    //        vidas--;
+    //        Debug.Log("Fallaste");
+
+    //        if (vidas <= 0)
+    //        {
+    //            Morir();
+    //        }
+    //    }
+    //    if (collision.gameObject.CompareTag("Incorrecto3")) //Que sucede con Incorrecto
+    //    {
+    //        vidas--;
+    //        Debug.Log("Fallaste");
+
+    //        if (vidas <= 0)
+    //        {
+    //            Morir();
+    //        }
+    //    }
+    //    if (collision.gameObject.CompareTag("Incorrecto4")) //Que sucede con Incorrecto
+    //    {
+    //        vidas--;
+    //        Debug.Log("Fallaste");
+
+    //        if (vidas <= 0)
+    //        {
+    //            Morir();
+    //        }
+    //    }
+    //    if (collision.gameObject.CompareTag("Incorrecto5")) //Que sucede con Incorrecto
+    //    {
+    //        vidas--;
+    //        Debug.Log("Fallaste");
+
+    //        if (vidas <= 0)
+    //        {
+    //            Morir();
+    //        }
     //    }
     //}
-
-    public void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Correcto1")) //Que sucede con Correcto
-        {
-            //Animacion de cortar
-            Destroy(collision.gameObject);
-            Debug.Log("Acertaste");
-        }
-        if (collision.gameObject.CompareTag("Correcto2")) //Que sucede con Correcto
-        {
-            //Animacion de cortar
-            Destroy(collision.gameObject);
-            Debug.Log("Acertaste");
-        }
-        if (collision.gameObject.CompareTag("Correcto3")) //Que sucede con Correcto
-        {
-            //Animacion de cortar
-            Destroy(collision.gameObject);
-            Debug.Log("Acertaste");
-        }
-        if (collision.gameObject.CompareTag("Correcto4")) //Que sucede con Correcto
-        {
-            //Animacion de cortar
-            Destroy(collision.gameObject);
-            Debug.Log("Acertaste");
-        }
-
-        if (collision.gameObject.CompareTag("Incorrecto1")) //Que sucede con Incorrecto
-        {
-            vidas--;
-            Debug.Log("Fallaste");
-
-            if (vidas <= 0)
-            {
-                Morir();
-            }
-        }
-        if (collision.gameObject.CompareTag("Incorrecto2")) //Que sucede con Incorrecto
-        {
-            vidas--;
-            Debug.Log("Fallaste");
-
-            if (vidas <= 0)
-            {
-                Morir();
-            }
-        }
-        if (collision.gameObject.CompareTag("Incorrecto3")) //Que sucede con Incorrecto
-        {
-            vidas--;
-            Debug.Log("Fallaste");
-
-            if (vidas <= 0)
-            {
-                Morir();
-            }
-        }
-        if (collision.gameObject.CompareTag("Incorrecto4")) //Que sucede con Incorrecto
-        {
-            vidas--;
-            Debug.Log("Fallaste");
-
-            if (vidas <= 0)
-            {
-                Morir();
-            }
-        }
-        if (collision.gameObject.CompareTag("Incorrecto5")) //Que sucede con Incorrecto
-        {
-            vidas--;
-            Debug.Log("Fallaste");
-
-            if (vidas <= 0)
-            {
-                Morir();
-            }
-        }
-    }
-    public void Morir()
-    {
-        if (vidas == 0)
-        {
-            canvasGameOver.SetActive(true);
-            Boton.gameObject.SetActive(false);
-        }
-    }
+    //public void Morir()
+    //{
+    //    if (vidas <= 0)
+    //    {
+    //        canvasGameOver.SetActive(true);
+    //        Boton.gameObject.SetActive(false);
+    //    }
+    //}
 
 }
---
+++ Assets/Scripts/Cocina/Cuchillo.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Cuchillo : MonoBehaviour
+{
+    public CocinaManager managerCocina;
+    string _zona;
+    public int ganar = 0;
+
+    [SerializeField] GameObject canvasGameOver;
+    [SerializeField] GameObject canvasWinGame;
+    [SerializeField] GameObject Boton;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvasGameOver.SetActive(false);
+        canvasWinGame.SetActive(false);
+        Boton.gameObject.SetActive(true);
+        _zona = string.Empty;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        _zona = collision.tag;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _zona = string.Empty;
+    }
+
+    public void Corte()
+    {
+        if(_zona != string.Empty)
+        {
+            Destroy(GameObject.FindGameObjectWithTag(_zona));
+            _zona = string.Empty;
+            ganar++;
+            Debug.Log("Acertaste");
+            if(ganar >= 4)
+            {
+                Ganaste();
+            }
+        }
+        else
+        {
+            managerCocina.vidas--;
+            Debug.Log("Fallaste");
+            if (managerCocina.vidas <= 0 )
+            {
+                Morir();
+            }
+        }
+    }
+    public void Morir()
+    {
+        if (managerCocina.vidas <= 0)
+        {
+            canvasGameOver.SetActive(true);
+            Boton.gameObject.SetActive(false);
+        }
+    }
+    public void Ganaste()
+    {
+        if (ganar >= 4)
+        {
+            canvasWinGame.SetActive(true);
+            Boton.gameObject.SetActive(false);
+        }
+    }
+}
+
