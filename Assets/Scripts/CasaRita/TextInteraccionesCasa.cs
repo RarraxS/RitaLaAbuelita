d7c2c831fe1c45897d01c96272f200b029566efc
informaticaesne <informatica@esne.es>
2024-01-23 15:52:18 +0100

    post beta

--- Assets/Scripts/CasaRita/TextInteraccionesCasa.cs
+++ Assets/Scripts/CasaRita/TextInteraccionesCasa.cs
@@ -1,20 +1,24 @@
 using UnityEngine;
+using System;
+using System.Drawing;
 
 public class TextInteraccionesCasa : MonoBehaviour
 {
     public Transform targetObject;  // Referencia al GameObject que seguirá el objeto de texto.
+    public Transform textTransform;
     private Vector3 distancia = new Vector3(525, 300, 0);
     private int velocidad = 2;
+    public Vector3 VerticalOffset;
 
     void Update()
     {
         if (targetObject != null)
         {
             // Calcula la posición a la que el texto debe seguir al objeto.
-            Vector3 targetPosition = targetObject.position + distancia;
+            Vector3 targetPosition = Camera.main.WorldToScreenPoint(targetObject.position);
 
             // Asigna la nueva posición al objeto de texto.
-            transform.position = targetPosition;
+            textTransform.position = targetPosition + VerticalOffset;
         }
     }
 }
