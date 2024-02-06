39372b7f84d6743e4528e130c8d5aebac2d29855
informaticaesne <informatica@esne.es>
2024-02-06 15:42:24 +0100

    Indicador menú principal terminado

--- Assets/Scripts/Titulo/Indicador.cs
+++ Assets/Scripts/Titulo/Indicador.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using static System.Net.Mime.MediaTypeNames;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
 
 public class Indicador : MonoBehaviour
 {
@@ -16,6 +19,28 @@ public class Indicador : MonoBehaviour
     {
         textIndcador.rectTransform.anchoredPosition = 
         Input.mousePosition / transform.localScale.x;
+
+        textIndcador.text = string.Empty;
+
+        // Check if the mouse was clicked over a UI element
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            PointerEventData pe = new PointerEventData(EventSystem.current);
+
+            pe.position = Input.mousePosition;
+            List<RaycastResult> hits = new List<RaycastResult>();
+
+            EventSystem.current.RaycastAll(pe, hits);
+
+            foreach (RaycastResult hit in hits)
+            {
+                if (hit.gameObject.CompareTag("toolTip"))
+                {
+                    textIndcador.text = hit.gameObject.name;
+                }
+            }
+        }
+
     }
 
     private void MostrarIndicador()
