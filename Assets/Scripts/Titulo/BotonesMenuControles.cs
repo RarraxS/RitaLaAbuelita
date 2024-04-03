3fcc73b96ad6a27010383c2dc6dc8dddf6ad7022
informaticaesne <informatica@esne.es>
2024-04-03 09:55:01 +0200

    Retocando la UI del inicio

--- Assets/Scripts/Titulo/BotonesMenuControles.cs
+++ Assets/Scripts/Titulo/BotonesMenuControles.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Menudefinitivo : MonoBehaviour
+public class BotonesMenuControles : MonoBehaviour
 {
     [SerializeField] GameObject canvasSonido;
     [SerializeField] GameObject canvasControles;
@@ -13,36 +13,36 @@ public class Menudefinitivo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        canvasSonido.SetActive(true);
         canvasControles.SetActive(false);
+        canvasSonido.SetActive(false);
         canvasPersonajes.SetActive(false);
-        canvasSalir.SetActive(false);
+        canvasSalir.SetActive(true);
     }
     public void Sonido()
     {
-        canvasSonido.SetActive(true);
         canvasControles.SetActive(false);
+        canvasSonido.SetActive(true);
         canvasPersonajes.SetActive(false);
         canvasSalir.SetActive(false);
     }
     public void Controles()
     {
-        canvasSonido.SetActive(false);
         canvasControles.SetActive(true);
+        canvasSonido.SetActive(false);
         canvasPersonajes.SetActive(false);
         canvasSalir.SetActive(false);
     }
     public void Personajes()
     {
-        canvasSonido.SetActive(false);
         canvasControles.SetActive(false);
+        canvasSonido.SetActive(false);
         canvasPersonajes.SetActive(true);
         canvasSalir.SetActive(false);
     }
      public void Salir()
     {
-        canvasSonido.SetActive(false);
         canvasControles.SetActive(false);
+        canvasSonido.SetActive(false);
         canvasPersonajes.SetActive(false);
         canvasSalir.SetActive(true);
     }
